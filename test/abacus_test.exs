d115b85e6d7ac89d4d7d08f47f83eef678eb99d4
xvw <xavier.vdw@gmail.com>
2017-04-05 15:11:51 +0200

    Remove printer

--- test/abacus_test.exs
+++ test/abacus_test.exs
@@ -1,14 +1,9 @@
 defmodule AbacusTest do
   use ExUnit.Case
-  use Abacus
-
-  base :cm
-  unit :km, 1000
 
   doctest Abacus
 
   test "the truth" do
-    IO.inspect (from (cm 1000), to: :km)
     assert 1 + 1 == 2
   end
 end
