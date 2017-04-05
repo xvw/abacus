4e769ff64359462a0d2046fa269bfdc058a78fda
xvw <xavier.vdw@gmail.com>
2017-04-05 14:44:58 +0200

    Initialize Abacus

--- lib/abacus.ex
+++ lib/abacus.ex
@@ -1,18 +1,48 @@
 defmodule Abacus do
+
   @moduledoc """
-  Documentation for Abacus.
+  Abacus is a tool to Abacus is a tool to simplify 
+  the handling of units.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Abacus.hello
-      :world
-
-  """
-  def hello do
-    :world
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+      import Abacus
+      @childs %{}
+      @base nil
+      @before_compile Abacus
+    end
   end
+
+  @doc false 
+  defmacro __before_compile__(_env) do
+  end
+
+  defexception message: "default message"
+
+  defmacro base(name) do 
+    quote do 
+      if @base do
+        raise Abacus, message: "Base is already defined"
+      end
+      @base unquote(name)
+      def unquote(name)(value), do: {unquote(name), value}
+    end
+  end
+
+  defmacro unit(name, expr) do
+    quote do
+      unless @base do 
+        raise Abacus, message: "Base must be defined"
+      end
+      unit_name  = unquote(name)
+      if @base == unit_name || Map.has_key?(@childs, unit_name) do 
+        raise Abacus, message: "#{unit_name} is already defined"
+      end
+      @childs Map.put_new(@childs, unit_name, unquote(expr))
+      def unquote(name)(value), do: {unquote(name), value}
+    end
+  end
+
 end
--- test/abacus_test.exs
+++ test/abacus_test.exs
@@ -1,8 +1,14 @@
 defmodule AbacusTest do
   use ExUnit.Case
+  use Abacus
+
+  base :cm
+  unit :km, 1000
+
   doctest Abacus
 
   test "the truth" do
+    IO.inspect km(1000)
     assert 1 + 1 == 2
   end
 end
